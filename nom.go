c9e7f90c7fcdd78e2027f3c4ce61b8d110054454
Thor Sideburns <thorsten.sideb0ard@gmail.com>
2015-06-23 08:02:27 -0700

    sum color

--- nom.go
+++ nom.go
@@ -2,11 +2,17 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"log"
 
-	"code.google.com/p/gopacket/dumpcommand"
-	"code.google.com/p/gopacket/pcap"
+	"github.com/google/gopacket"
+	"github.com/google/gopacket/pcap"
+	"github.com/mgutz/ansi"
 )
+
+var colors = []string{ansi.ColorCode("red+b:white"), ansi.ColorCode("white+h:blue")}
+var reset = ansi.ColorCode("reset")
+var toggle = 1
 
 func main() {
 	flag.Parse()
@@ -23,5 +29,36 @@ func main() {
 	}
 	defer handle.Close()
 
-	dumpcommand.Run(handle)
+	packetSource := gopacket.NewPacketSource(handle, handle.LinkType())
+
+	for pkt := range packetSource.Packets() {
+		//fmt.Println("Pkt is:", reflect.TypeOf(pkt))
+		nom(pkt)
+		//eth, _ := layers.LayerTypeEthernet.(*layers.Ethernet)
+		//fmt.Println(eth.SrcMac, eth.DstMac)
+		//if ethLayer := pkt.Layer(layers.LayerTypeEthernet); ethLayer != nil {
+		//	fmt.Println("Ethernet")
+		//}
+
+		//if ipLayer := pkt.Layer(layers.LayerTypeIPv4); ipLayer != nil {
+		//	fmt.Println("IP")
+		//	if tcpLayer := pkt.Layer(layers.LayerTypeTCP); tcpLayer != nil {
+		//		// Get actual TCP data from this layer
+		//		tcp, _ := tcpLayer.(*layers.TCP)
+		//		fmt.Printf("From src port %d to dst port %d\n", tcp.SrcPort, tcp.DstPort)
+		//	}
+		//}
+		//nom(pkt)
+		//for _, layer := range pkt.Layers() {
+		//	//fmt.Println("PACKET LAYER:", layer.LayerType())
+		//	fmt.Println("PACKET LAYER:", reflect.TypeOf(layer))
+		//}
+	}
+
+}
+
+func nom(pkt gopacket.Packet) {
+	fmt.Println(colors[toggle], pkt, reset)
+	toggle = 1 - toggle
+	fmt.Println()
 }
